c6f2111bb93de9991e6e6efc84fa936ae84977df
Carlos Eduardo Navarro Racines <c.e.navarro@cgiar.org>
2026-06-11 07:39:25 -0700

    anomalies for hnd

--- 03_Anomalies_Calcs_CMIP6_hnd.R
+++ 03_Anomalies_Calcs_CMIP6_hnd.R
@@ -6,16 +6,21 @@
 require(raster)
 #require(ncdf)
 #require(maptools)
-#require(rgdal)
+require(rgdal)
+library(terra)
+library(sf)
 
 ## Parameters
 dDir <- "Z:/1.Data/Results/climate/02_climate_change/hnd_2_5min"
 bDir <- "Z:/1.Data/Results/climate/01_baseline/hnd/average_v2"
 scn_list <- c("ssp_126", "ssp_245", "ssp_370", "ssp_585")
-# scn_list <- "ssp_585"
 perList <- c("2030s", "2050s", "2070s") #, "2090s")
 varList <- c("prec", "tmax", "tmin", "tmean")
 seasons <- list("djf"=c(12,1,2), "mam"=3:5, "jja"=6:8, "son"=9:11, "ann"=1:12)
+#msk <- as(vect("Z:/1.Data/Results/climate/00_admin_data/gadm41_HND_0.shp"), "Spatial")
+mask <- raster("Z:/1.Data/Results/climate/00_admin_data/hnd_msk_30s.tif")
+scn_list <- "ssp_585"
+
 
 for (scn in scn_list){
   
@@ -29,7 +34,7 @@ for (scn in scn_list){
       
       cat("Anomalies calcs over: ", scn, gcm, period, "\n")
       
-      oDir <- paste0(dDir, "_anom/", scn, "/", gcm, "/", period)
+      oDir <- paste0(gsub("2_5min", "30s", dDir), "_anom/", scn, "/", gcm, "/", period)
       if (!file.exists(oDir)) {dir.create(oDir, recursive=T)}
       
       for (var in varList){
@@ -38,11 +43,23 @@ for (scn in scn_list){
           
           if (!file.exists(paste(oDir, "/", var, "_", mth,".tif", sep=""))) {
             
-            bsl <- raster(paste0(bDir, "/", var, "_",mth, ".tif"))
+            bsl <- raster(paste0(bDir, "/", var, "_",mth, ".asc"))
             del <- raster(paste0(dDir, "/", scn, "/", gcm, "/", period, "/", var, "_", mth, ".tif"))
             
+            bsl <- mask(crop(bsl, mask), mask)
+            del <- resample(del, mask, method = "bilinear")
+            
+            bsl_adj <- bsl
+            bsl_adj[bsl_adj < 10] <- 10   # piso mínimo de 5 mm
+            
             if (var == "prec"){
-              anom <- del/bsl - 1
+              
+              #anom <- del/bsl - 1
+              eps <- 0.01   # mm
+              anom <- del / (bsl + eps) - 1
+              anom[anom > 1] <- 0     # máximo +100%
+              anom[anom < -1] <- 0   # mínimo -100%
+              
             } else {
               anom <- del - bsl
             }
