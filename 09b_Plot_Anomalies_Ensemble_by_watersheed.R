a34bcf6025872be3614569493db76e6130d7e70a
Carlos Eduardo Navarro Racines <c.e.navarro@cgiar.org>
2026-06-15 08:37:10 -0700

    updates

--- 09b_Plot_Anomalies_Ensemble_by_watersheed.R
+++ 09b_Plot_Anomalies_Ensemble_by_watersheed.R
@@ -11,24 +11,24 @@ require(raster)
 require(maptools)
 require(latticeExtra)
 require(RColorBrewer)
-
+library(terra)
+library(sf)
 
 ######################################
 #### 01 Plots anomalies by months ####
 ######################################
 
-sspList <- c("ssp_126", "ssp_245", "ssp_585") #"ssp_370"
+sspList <- c("ssp_245", "ssp_585") #"ssp_126", "ssp_370"
 #sspList <- c("ssp_585")
-baseDir <- "D:/cenavarro/msc_gis_thesis/02_climate_change/camexca_2_5min_anom_ens"
-# baseDir <- "D:/cenavarro/msc_gis_thesis/02_climate_change/camexca_2_5min_anom"
-perList <- c("2030s", "2050s") #, "2070s") #, "2090s")
+baseDir <- "Z:/1.Data/Results/climate/02_climate_change/pan_30s_anom_ens"
+perList <- c("2050s", "2070s") #, "2090s") #,"2030s", )
 varList <- c("prec", "tmin", "tmax", "tmean") 
 id <- c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic")
 # mask <- readOGR("D:/cenavarro/msc_gis_thesis/00_admin_data/CAMEXCA_adm0.shp")
 # mask <- readOGR("D:/cenavarro/msc_gis_thesis/00_admin_data/by_country/CRI_adm0.shp")
-mask <- readOGR("S:/admin_boundaries/shp_files/CRI_adm/CRI0.shp")
-mask_adm1 <- readOGR("S:/admin_boundaries/shp_files/CRI_adm/CRI1.shp")
-oDir <- "D:/cenavarro/bid_mag_pasar/climate_change/anomalies"
+mask <- as(project(vect("Z:/1.Data/Process/Info_Inputs_SWAT/Panama/Tonosi_La_Villa/Division_administrativa/Tonosi_la_Villa_corregimientos.shp"), crs_ref), "Spatial")
+mask_adm1 <- as(project(vect("Z:/1.Data/Process/Info_Inputs_SWAT/Panama/Tonosi_La_Villa/Division_administrativa/Tonosi_la_Villa_corregimientos.shp"), crs_ref), "Spatial")
+oDir <- "Z:/1.Data/Results/climate/02_climate_change/pan_evaluations"
 metrics <- c("avg") #, "q25", "q75")
 
 if (!file.exists(oDir)) {dir.create(oDir, recursive=T)}
@@ -53,13 +53,13 @@ for (ssp in sspList) {
           if (var == "prec"){
             
             stk_crop <- stk * 100
-            stk_crop[stk_crop > 20] = 20
-            stk_crop[stk_crop < (-20)] = (-20)
+            stk_crop[stk_crop > 60] = 60
+            stk_crop[stk_crop < (-60)] = (-60)
             
             plot <- setZ(stk_crop, id)
             names(plot) <- id
             
-            zvalues <- seq(-20, 20, 5) # Define limits
+            zvalues <- seq(-60, 60, 5) # Define limits
             myTheme <- BuRdTheme() # Define squeme of colors
             # myTheme$regions$col=colorRampPalette(c("#b2182b", "#d6604d", "#f4a582", "#f7f7f7","#d1e5f0","#92c5de","#4393c3","#2166ac", "#053061"))(length(zvalues)-1) # Set new colors
             myTheme <- rasterTheme(region=brewer.pal('RdBu', n=9))
@@ -69,12 +69,14 @@ for (ssp in sspList) {
           } else {
             
             stk_crop <- stk
-            stk_crop[stk_crop > 4 ] = 4
+            stk_crop[stk_crop > 5 ] = 5
+            stk_crop[stk_crop < 0 ] = 0
+            
             
             plot <- setZ(stk_crop, id)
             names(plot) <- id
             
-            zvalues <- seq(0, 4, 0.5)
+            zvalues <- seq(0, 5, 0.2)
             # zvalues <- c(0, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6)
             myTheme <- BuRdTheme()
             # myTheme$regions$col=colorRampPalette(c("white", "snow","yellow","orange", "red", "darkred"))(length(zvalues)-1)
@@ -86,13 +88,13 @@ for (ssp in sspList) {
           } 
           
           # Save to file
-          tiff(oPlot, width=1400, height=1200, pointsize=8, compression='lzw',res=200)
-          print(levelplot(plot, at = zvalues, scales = list(draw=FALSE), layout=c(4, 3), xlab="", ylab="", par.settings = myTheme, 
+          tiff(oPlot, width=1600, height=800, pointsize=8, compression='lzw',res=200)
+          print(levelplot(plot, at = zvalues, scales = list(draw=FALSE), layout=c(6, 2), xlab="", ylab="", par.settings = myTheme, 
                           colorkey = list(space = "bottom", width=1.2, height=1)
           ) 
-          + layer(sp.polygons(mask_adm1, lwd=0.7))
-
+          + layer(sp.polygons(mask_adm1, lwd=0.8))
           )
+          
           dev.off()
           
         }
@@ -109,13 +111,13 @@ for (ssp in sspList) {
         
         if (var == "prec"){
           
-          stk_crop <- stkStd * 100
-          stk_crop[stk_crop > 40] = 40
+          stk_crop <- stkStd * 120
+          stk_crop[stk_crop > 120] = 120
           
           plot <- setZ(stk_crop, id)
           names(plot) <- id
           
-          zvalues <- seq(0, 40, 5) # Define limits
+          zvalues <- seq(0, 120, 5) # Define limits
           myTheme <- BuRdTheme() # Define squeme of colors
           
           
@@ -139,12 +141,14 @@ for (ssp in sspList) {
         myTheme$axis.line$col = 'white' # Eliminate frame from maps
         
         # Save to file
-        tiff(oPlot, width=1400, height=1200, pointsize=8, compression='lzw',res=200)
-        print(levelplot(plot, at = zvalues, scales = list(draw=FALSE), layout=c(4, 3), xlab="", ylab="", par.settings = myTheme, 
+        tiff(oPlot, width=1600, height=800, pointsize=8, compression='lzw',res=200)
+        
+        print(levelplot(plot, at = zvalues, scales = list(draw=FALSE), layout=c(6, 2), xlab="", ylab="", par.settings = myTheme, 
                         colorkey = list(space = "bottom", width=1.2, height=1)
         ) 
-        + layer(sp.polygons(mask_adm1, lwd=0.7))
+        + layer(sp.polygons(mask_adm1, lwd=0.8))
         )
+        
         dev.off()
         
       }
@@ -163,9 +167,6 @@ for (ssp in sspList) {
 
 seasons <- c("djf", "mam", "jja", "son", "ann")
 id <- rep(c("DEF ", "MAM", "JJA", "SON", "ANUAL"), length(perList))
-varList <- c("prec", "tmin", "tmax", "tmean") 
-sspList <- c("ssp_126", "ssp_245", "ssp_585") #"ssp_370"
-# sspList <- c("ssp_585")
 
 for (ssp in sspList) {
   
@@ -186,13 +187,13 @@ for (ssp in sspList) {
         if (var == "prec"){
           
           stk_crop <- stk * 100
-          stk_crop[stk_crop > 20] = 20
-          stk_crop[stk_crop < (-20)] = (-20)
+          stk_crop[stk_crop > 60] = 60
+          stk_crop[stk_crop < (-60)] = (-60)
           
           plot <- setZ(stk_crop, id)
           names(plot) <- id
           
-          zvalues <- seq(-20, 20, 5) # Define limits
+          zvalues <- seq(-60, 60, 5) # Define limits
           myTheme <- BuRdTheme() # Define squeme of colors
           myTheme <- rasterTheme(region=brewer.pal('RdBu', n=9))
           myTheme$strip.border$col = "white" # Eliminate frame from maps
@@ -201,12 +202,13 @@ for (ssp in sspList) {
         } else {
           
           stk_crop <- stk
-          stk_crop[stk_crop > 4 ] = 4
+          stk_crop[stk_crop < 0 ] = 0
+          stk_crop[stk_crop > 5 ] = 5
           
           plot <- setZ(stk_crop, id)
           names(plot) <- id
           
-          zvalues <- seq(0, 4, 0.5)
+          zvalues <- seq(0, 5, 0.2)
           # zvalues <- c(0, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6)
           myTheme <- BuRdTheme()
           myTheme=rasterTheme(region=brewer.pal('YlOrRd', n=9))
@@ -217,7 +219,7 @@ for (ssp in sspList) {
         } 
         
         
-        tiff(oPlot, width=2000, height=900, compression='lzw',res=200)
+        tiff(oPlot, width=1600, height=900, compression='lzw',res=200)
         
         print(levelplot(plot, at = zvalues,  
                         scales = list(draw=FALSE), 
@@ -254,12 +256,12 @@ for (ssp in sspList) {
       if (var == "prec"){
         
         stk_crop <- stk * 100
-        stk_crop[stk_crop > 30] = 30
+        stk_crop[stk_crop > 120] = 120
 
         plot <- setZ(stk_crop, id)
         names(plot) <- id
         
-        zvalues <- seq(0, 30, 5) # Define limits
+        zvalues <- seq(0, 120, 5) # Define limits
 
         
       } else {
@@ -279,7 +281,7 @@ for (ssp in sspList) {
       myTheme$strip.border$col = "white" # Eliminate frame from maps
       myTheme$axis.line$col = 'white' # Eliminate frame from maps
       
-      tiff(oPlot, width=2000, height=900, compression='lzw',res=200)
+      tiff(oPlot, width=1600, height=900, compression='lzw',res=200)
       
       print(levelplot(plot, at = zvalues,  
                       scales = list(draw=FALSE), 
---
+++ 12_Extract_by_points.R
@@ -0,0 +1,66 @@
+### Author : Carlos Navarro c.e.navarro@cgiar.org
+### Date : Jan 2016
+
+library(raster)
+library(terra)
+library(sp)
+
+####################
+#### 02 Future  ####
+####################
+
+bDir <- "Z:/1.Data/Results/climate/02_climate_change/pan_30s_anom_ens"
+
+pts <- vect("Z:/1.Data/Process/Info_Inputs_SWAT/Republica_Dominicana/Cambio_Climatico/Centroides_Subcuencas.shp")
+
+oDir <- "Z:/1.Data/Process/Info_Inputs_SWAT/Panama/Tonosi_La_Villa/Cambio_Climatico"
+
+varList <- c("prec", "tmin", "tmax", "tmean")
+varListMod <- c("pr", "tn", "tx", "tm")
+
+sspList <- c("ssp_245", "ssp_585")
+perList <- c("2050s", "2070s")
+ext <- "tif"
+
+ref <- "Z:/1.Data/Results/climate/02_climate_change/pan_30s_anom_ens/ssp_245/2050s/prec_1_avg.tif"
+
+pts <- project(pts, crs(rast(ref)))
+
+# Convertir a Spatial para seguir usando raster::extract()
+pts <- as(pts, "Spatial")
+
+for (ssp in sspList) {
+  
+  for (per in perList) {
+    
+    for (var in 1:length(varList)) {
+      
+      # Stack by variables
+      stk <- stack(paste0(
+        bDir, "/", ssp, "/", per, "/", 
+        varList[var], "_", 1:12, "_avg.", ext
+      ))
+      
+      # point shapefile
+      vals <- round(extract(stk, pts), 4)
+      
+      ssp_col <- gsub("_", "", ssp)
+      
+      colnames(vals) <- paste0(
+        varListMod[var],
+        substring(ssp_col, nchar(ssp_col) - 1),
+        "_",
+        substr(per, nchar(per) - 2, nchar(per)),
+        1:12
+      )
+      
+      pts@data <- cbind(pts@data, vals)
+    }
+  }
+}
+
+writeVector(
+  vect(pts),
+  filename = file.path(oDir, "Centroides_Subcuencas_vals.shp"),
+  overwrite = TRUE
+)
